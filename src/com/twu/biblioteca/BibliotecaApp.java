312ed5274413441d0d38e15f8553a8a7c24d9d63
Moritz Dück <moritz.dueck@thoughtworks.com>
2020-01-29 16:24:09 +0100

    [Moritz Dueck] [1.2] print all books on startup

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,5 +8,12 @@ public class BibliotecaApp {
 
     public void start() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        printAllBooks();
+    }
+
+    public void printAllBooks() {
+        System.out.println("Clean Code: A Handbook of Agile Software Craftsmanship");
+        System.out.println("The Pragmatic Programmer: From Journeyman to Master");
+        System.out.println("Code Complete: A Practical Handbook of Software Construction");
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTests.java
+++ test/com/twu/biblioteca/BibliotecaAppTests.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import java.io.PrintStream;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class BibliotecaAppTests {
 
@@ -22,4 +21,19 @@ public class BibliotecaAppTests {
         verify(out).println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 
+    @Test
+    public void shouldListAllBooksOnStart() {
+
+        //given
+        PrintStream out = mock(PrintStream.class);
+        System.setOut(out);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        //when
+        bibliotecaApp.start();
+        //then
+        verify(out).println("Clean Code: A Handbook of Agile Software Craftsmanship");
+        verify(out).println("The Pragmatic Programmer: From Journeyman to Master");
+        verify(out).println("Code Complete: A Practical Handbook of Software Construction");
+
+    }
 }
